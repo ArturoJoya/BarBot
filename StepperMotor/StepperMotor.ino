689d3751dae80aef5fb103322c80e095d6ad4f3a
Arturo Joya <98101322+ArturoJoya@users.noreply.github.com>
2022-12-03 00:44:26 -0500

    Update Test file

--- StepperMotor/StepperMotor.ino
+++ StepperMotor/StepperMotor.ino
@@ -6,13 +6,19 @@
 #define stepPin 3
 #define dir2Pin 4
 #define step2Pin 5
+#define dir3Pin 7
+#define step3Pin 6
+#define dir4Pin 8
+#define step4Pin 9
 #define mit 1
-#define reset 6
+#define reset 8
 
 #define stepsPerRevolution 200
 
 AccelStepper pump1 = AccelStepper(mit, stepPin, dirPin);
 AccelStepper pump2 = AccelStepper(mit, step2Pin, dir2Pin);
+AccelStepper pump3 = AccelStepper(mit, step3Pin, dir3Pin);
+AccelStepper pump4 = AccelStepper(mit, step4Pin, dir4Pin);
 
 void setup() {
   // Declare pins as output:
@@ -25,6 +31,14 @@ void setup() {
   pump2.setAcceleration(50);
   pump2.setSpeed(200);
   pump2.moveTo(5000000);
+  pump3.setMaxSpeed(1000);
+  pump3.setAcceleration(50);
+  pump3.setSpeed(200);
+  pump3.moveTo(5000000);
+  pump4.setMaxSpeed(1000);
+  pump4.setAcceleration(50);
+  pump4.setSpeed(200);
+  pump4.moveTo(5000000);
 
   pinMode(reset, INPUT);
   
@@ -40,12 +54,22 @@ void loop() {
     pump2.moveTo(-pump2.currentPosition());
     Serial.println("Rotating Motor in opposite direction...");
   }
+  if (pump3.distanceToGo() == 0) {
+    pump3.moveTo(-pump3.currentPosition());
+    Serial.println("Rotating Motor in opposite direction...");
+  }
+  if (pump4.distanceToGo() == 0) {
+    pump4.moveTo(-pump4.currentPosition());
+    Serial.println("Rotating Motor in opposite direction...");
+  }
   if(digitalRead(reset) == HIGH){
     while(digitalRead(reset) == HIGH){}
     pump1.setSpeed(0);
     pump2.setSpeed(0);
   }
   pump1.run();
-  pump2.run();
+  //pump2.run();
+  pump3.run();
+  //pump4.run();
 
 }
